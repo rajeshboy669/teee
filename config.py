abc03047336c489d285f4e37ece18532bea6d178
Jit <142555479+jit362@users.noreply.github.com>
2024-12-08 06:03:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Default configuration for the URL shortener bot
 DEFAULT_API_URL = "https://indishort.live/api"
 DEFAULT_SITE_URL = "indishort.live"
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+BOT_TOKEN = "7848736443:AAEiLgkWT8xriEMaffW4mZnj_GyiTtWq8AE"
