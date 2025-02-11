5032476a8371defa0f4f365ce5bbca14c239b9b8
ᴊɪᴛᴛᴜ ʙʜᴀɪ <142555479+jit362@users.noreply.github.com>
2025-02-11 08:12:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 # Default configuration for the URL shortener bot
 DEFAULT_API_URL = "https://indishort.live/api"
 DEFAULT_SITE_URL = "indishort.live"
-BOT_TOKEN = "7866347129:AAFy6CiE2wQcAKeOgIBqJ3zTA5OMkxSUMGE"
-class Config:
-    PORT = 8080
-
+BOT_TOKEN = "7912197218:AAECHrYc45oPfOfsr7Y97k1Chf6ShzBpGs4"
