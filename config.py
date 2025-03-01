9a8430d38899795c3702e5c41b7fa241ab76f9cf
rajeshboy669 <rajeshgautam.1077@gmail.com>
2025-03-01 16:21:46 +0545

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Default configuration for the URL shortener bot
-DEFAULT_API_URL = "https://indishort.live/api"
-DEFAULT_SITE_URL = "indishort.live"
-BOT_TOKEN = "7912197218:AAECHrYc45oPfOfsr7Y97k1Chf6ShzBpGs4"
+DEFAULT_API_URL = "https://npshort.com/api"
+DEFAULT_SITE_URL = "npshort.com"
+BOT_TOKEN = "7912197218:15d6a05d79d9074c1b44d5ef365626e0625dab90"
